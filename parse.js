64e00732812a3784d91a77e318f41590ec4ecb04
Christian Brosche <c.brosche@gmail.com>
2017-02-09 23:14:24 +0100

    Trying out cheerio - looks promising.

--- parse.js
+++ parse.js
@@ -10,22 +10,22 @@ function logNested(object) {
     console.log(util.inspect(object, {showHidden: false, depth: null}));
 }
 
-const parser = new parse5.SAXParser();
-
-parser.on('startTag', (text, attrs) => {
-    console.log(text);
-    console.log(attrs);
-});
-
 function sendStationRequest(station) {
     const requestUrl = 'http://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle=' + station;
-    http.get(requestUrl, res => {        
-        res.pipe(parser);
+    request(requestUrl, (error, response, body) => {
+        const document = parse5.parse(body);
+
+        const station = getOriginStation(document);
+
+     
+        console.log(station);
+
+        const destinations = getDestinations(document);   
     });
 }
 
-function logComplex(document) {
-    logNested(document
+function getOriginStation(document) {
+    return document
         .childNodes[1] //html
         .childNodes[2] //body
         .childNodes[3] //table departureTable departureView
@@ -33,7 +33,69 @@ function logComplex(document) {
         .childNodes[0] //tr
         .childNodes[1] //td headerStationColumn
         .childNodes[0].value
-    );
+}
+
+function getDestinations(document) {
+    console.log("destinations");
+
+    document
+        .childNodes[1]
+        .childNodes[2]
+        .childNodes[3]
+        .childNodes[1]
+        .childNodes
+        .map(childNode => {
+            console.log("childNode:")
+            if (childNode.childNodes != undefined) {
+                
+                console.log(childNode.childNodes[1].childNodes[1].childNodes[0].value)
+            }
+            
+        });
+
+    // Number
+    console.log(document
+        .childNodes[1] //html
+        .childNodes[2] //body
+        .childNodes[3] //table departureTable departureView
+        .childNodes[1] //tbody
+        .childNodes[2] //tr
+        .childNodes[1] //td lineColumn
+        .childNodes[0]
+        .value
+    )
+
+    // Destination
+    console.log(document
+        .childNodes[1] //html
+        .childNodes[2] //body
+        .childNodes[3] //table departureTable departureView
+        .childNodes[1] //tbody
+        .childNodes[2] //tr
+        .childNodes[3] //td lineColumn
+        .childNodes[0]
+        .value.trim()
+    )
+  
+  // Time until
+  console.log(document
+        .childNodes[1] //html
+        .childNodes[2] //body
+        .childNodes[3] //table departureTable departureView
+        .childNodes[1] //tbody
+        .childNodes[2] //tr
+        .childNodes[5] //td lineColumn
+        .childNodes[0]
+        .value
+    )
+    
+        
+        
+        /*
+        .childNodes.map( childNode => {
+            return childNode
+        })
+        */
 }
 
 sendStationRequest('Harras');
