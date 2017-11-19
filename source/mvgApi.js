a38b58dd66a8e1ca7f613e3790679330f76e596b
Christian Brosche <c.brosche@gmail.com>
2017-11-19 19:43:45 +0100

    Removed old parser code and implemented first version of module using JSON endpoint.

--- source/mvgApi.js
+++ source/mvgApi.js
@@ -5,100 +5,181 @@ const Line = require("./Line");
 const iconv = require("iconv-lite");
 const defaultApi = "https://www.mvg-live.de/ims/dfiStaticAuswahl.svc";
 
+const mvgHeader = {
+    "X-MVG-Authorization-Key": "5af1beca494712ed38d313714d4caff6"
+}
+
 /**
- * Get the departures for a certain station.
- *
- * @station String The name of the station you want to get the departures for.
- * @options Array An array of the types of transportation you want to have shown.
- *          Can be one or more of the following:
- *          "ubahn", "sbahn", "tram", "bus"
- *          If not provided, departures for all kinds of transit are shown.
- * @apiUrl  String for custom API endpoint. If not provided, defaultApi is used
- */
-function getDepartures(station, options, apiUrl = defaultApi) {
-
-    let transitParams = buildTransitParams(options);
-
-    const requestUrl = apiUrl.concat('?haltestelle=').concat(station).concat(transitParams);
-
-    const linesPromise = new Promise((resolve, reject) => {
-        request.get({uri: requestUrl, encoding: null}, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-
-                const cheerioPage = initCheerio(body);
-                const lines = cheerioPage('.rowEven, .rowOdd');
-                const linesArr = extractLineInfo(lines, cheerioPage);
-                const linesSorted = sortLines(linesArr);
-
-                resolve(linesSorted);
-            } else if (!error) {
-                console.log("Connection error, status code: " + response.statusCode);
-                reject(response.statusCode);
-            } else {
-                console.log("Error: " + error);
-                reject(error);
-            }
-        });
-    });
-
-    return linesPromise;
-}
-
-function buildTransitParams(options) {
-    let transitParams = "";
-    if (typeof options != 'undefined') {
-        options.forEach(transitType => {
-            transitParams = transitParams.concat("&").concat(transitType).concat("=checked");
-        });
+ * Departure endpoint. Returns Json in the form of:
+ * 
+ * {
+        "departures": [
+            {
+                "departureTime": 1510687290000,
+                "product": "b",
+                "label": "X30",
+                "destination": "Ostbahnhof",
+                "live": true,
+                "lineBackgroundColor": "#5c947d",
+                "departureId": -1297847819,
+                "sev": false
+            },
+            ...
+        ]
     }
-    return transitParams;
+ */
+const departureIdEndpoint = (id) => `https://www.mvg.de/fahrinfo/api/departure/${id}?footway=0`;
+
+const stationNameEndpoint = (name) => `https://www.mvg.de/fahrinfo/api/location/queryWeb?q=${name}`;
+
+/**
+ * Station endpoint. Can be used with IDs or station names.
+ * 
+ * Returns Json in the form of:
+ * 
+ * {
+        "locations": [
+            {
+                "type": "station",
+                "latitude": 48.116876,
+                "longitude": 11.536213,
+                "id": 1130,
+                "place": "München",
+                "name": "Harras",
+                "hasLiveData": false,
+                "hasZoomData": true,
+                "products": [
+                    "b",
+                    "u",
+                    "s"
+                ],
+                "lines": {
+                    "tram": [],
+                    "nachttram": [],
+                    "sbahn": [
+                        "7"
+                    ],
+                    "ubahn": [
+                        "6"
+                    ],
+                    "bus": [
+                        "53",
+                        "54",
+                        "130",
+                        "132",
+                        "134",
+                        "X30"
+                    ],
+                    "nachtbus": [
+                        "40",
+                        "41"
+                    ],
+                    "otherlines": []
+                }
+            }
+        ]
+    }
+ */
+const stationEndpoint = (identifier) => `https://www.mvg.de/fahrinfo/api/location/query?q=${identifier}`;
+
+
+function getStationByName(stationName) {
+    const requestUri = stationEndpoint(stationName);
+
+    return requestPromise(requestUri)
+    .then(requestBody => handleJSON(requestBody))    
+    .then(jsonBody => getStationFromJSON(jsonBody));
 }
 
-function initCheerio(body) {
-    const utf8string = convertIsoResponseToUtf8(body);
-    return cheerio.load(utf8string);
-}
-
-function convertIsoResponseToUtf8(body) {
-    return iconv.decode(new Buffer(body), "ISO-8859-1");
-}
-
-function extractLineInfo(lines, cheerioPage) {
-    const linesArr = [];
-    lines.map((index, element) => {
-        const cheerioElement = cheerioPage(element);
-        const line = getLineFromCheerioElement(cheerioElement);
-        linesArr.push(line);
+function getStationFromJSON(jsonBody) {
+    return new Promise((resolve, reject) => {
+        try {
+            const station = jsonBody.locations[0];
+            resolve(station);
+        } catch (exception) {
+            console.log(`JSON wasn't formatted as expected: ${exception}`);
+            reject(exception);
+        }     
     });
-    return linesArr;
 }
 
-function getLineFromCheerioElement(cheerioElement) {
-    const lineNumber = getLineNumber(cheerioElement);
-    const lineName = getDestinationName(cheerioElement);
-    const lineDepartureIn = getLineDepartureIn(cheerioElement);
-
-    return new Line(lineNumber, lineName, lineDepartureIn)
+function requestPromise(requestUri) {
+    return new Promise((resolve, reject) => {
+        request.get({uri: requestUri, encoding: null, headers: mvgHeader}, (error, response, body) => {
+            handleRequest(error, response, body, resolve, reject);
+        });
+    });
 }
 
-function getLineNumber(cheerioElement) {
-    return cheerioElement.find('.lineColumn').text()
+function handleJSON(requestBody) {
+    return new Promise((resolve, reject) => {
+        try{
+            resolve(JSON.parse(requestBody));
+        } catch (syntaxException) {
+            console.log(`Could not parse JSON because of invalid syntax: ${syntaxException}`);
+            reject(syntaxException)
+        }
+    });
 }
 
-function getDestinationName(cheerioElement) {
-    return cheerioElement.find('.stationColumn').text().trim()
+function getDeparturesById(stationId, options) {
+    const requestUri = departureIdEndpoint(stationId);
+
+    return requestPromise(requestUri)
+    .then(requestBody => handleJSON(requestBody))
+    .then(jsonBody => getDeparturesFromJSON(jsonBody));
 }
 
-function getLineDepartureIn(cheerioElement) {
-    return cheerioElement.find('.inMinColumn').text();
+function getDeparturesFromJSON(jsonBody) {
+    return new Promise((resolve, reject) => {
+        const departures = jsonBody.departures;
+        departures.map(departure => {
+            departure.departureIn = calculateTimeOffset(departure.departureTime);
+        });
+        if (typeof(options) != "undefined") {
+            const filteredDepartures = departures.filter(departure => {                            
+                return options.includes(departure.product);
+            });
+            resolve(filteredDepartures);
+        } else {
+            resolve(departures);
+        } 
+    });
 }
 
-function sortLines(linesArr) {
-    return linesArr.sort(compareLines);
+function handleRequest(error, response, body, resolve, reject) {
+    if (successfulRequest(error, response)) {
+        resolve(body)
+    } else if (!error) {
+        console.log(`Connection error, status code: ${response.statusCode}.`);
+        reject(response.statusCode);
+    } else {
+        console.log(`Error: ${error}`);
+        reject(error);
+    }
 }
 
-function compareLines(lineA, lineB) {
-    return lineA.lineDepartureIn - lineB.lineDepartureIn;
+function successfulRequest(error, response) {
+    return !error && response.statusCode == 200;
+}
+
+function getStationId(stationName) {
+    return getStationByName(stationName).then(station => station.id);
+}
+
+function calculateTimeOffset(time) {
+    return Math.round((time - Date.now()) / 60000);
+}
+
+function getDepartures(stationName, options) {
+    const departuresPromise = getStationId(stationName)
+    .then(stationId => {
+            return getDeparturesById(stationId, options);
+    })
+    return departuresPromise;
 }
 
 exports.getDepartures = getDepartures;
+exports.getDeparturesById = getDeparturesById;
+exports.getStationId = getStationId;
+exports.getStationByName = getStationByName;
